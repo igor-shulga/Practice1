94fd0e68745d66f248072688aedba28d4c8e0c31
igor-shulga <igorshulga@yahoo.com>
2016-04-05 20:30:57 +0300

    111

--- src/main/java/SeleniumTest.java
+++ src/main/java/SeleniumTest.java
@@ -1,30 +1,48 @@
 import junit.framework.TestCase;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTest extends TestCase {
 
     private WebDriver driver = new FirefoxDriver();
 
     private By input = By.id("lst-ib");
+    private By result = By.id("resultStats");
 
     @BeforeClass
     public void setUp() throws Exception{
         this.driver.get("http://google.com");
     }
-    public void test() throws Exception{
+    @Test
+    public void testGoogleResult() throws Exception{
         System.out.println(this.driver.getCurrentUrl());
 
-       WebElement googleSearch = driver.findElement(input);
+        WebElement googleSearch = driver.findElement(input);
         googleSearch.sendKeys("Selenium IDE \n");
         System.out.println(googleSearch.getLocation());
         System.out.println(driver.getTitle());
 
-        assertTrue("Wrong message",driver.getTitle().contains("Google"));
+     //   WebDriverWait wait = new WebDriverWait(driver,15);
+       // wait.until(ExpectedCondition);
+
+
+        assertTrue("Wrong message", driver.getTitle().contains("Selenium IDE"));
+
+
+
+        WebElement resultStatus =driver.findElement(result);
+        assertTrue(resultStatus.isDisplayed());
+
+
+
+
 
         Thread.sleep(2000);
 
